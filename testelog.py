4f7ed783533362b8acc143db21398d16c3e0d6d2
Isadora <152524005+isaakirsch@users.noreply.github.com>
2024-11-19 14:12:33 -0300

    Update testelog.py

--- testelog.py
+++ testelog.py
@@ -316,7 +316,7 @@ def conectar_bd():
         conexao = mysql.connector.connect(
             host='dbava.c3s2ysmoum5c.us-east-2.rds.amazonaws.com', 
             database='ava',
-            user='ISADORA',
+            user='isadora',
             password='kirsch06',
             port=3306 
         )
